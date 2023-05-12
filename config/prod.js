d125e9258fccc15a9312bbb469c77b1e3c741e34
Muhammad <69915958+MuhammadSanaullah123@users.noreply.github.com>
2023-05-12 19:33:47 +0500

    Delete config directory

--- config/prod.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  mongoURI:
-    "mongodb+srv://Muhammad:Muhammad@unitify.h0cbjkg.mongodb.net/?retryWrites=true&w=majority",
-  jwtSecret: "mysecrettoken89216",
-  SENDGRID_API:
-    "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",
-  STRIPE_KEY:
-    "sk_test_51MjpgmSBz4Qe2gWIRnvf67dO836ht7JBJ1qQhL6PVuKDjbDoXSH7jX7z8MfucXhK25uQEZH9WeTFI9TrTkav5IXp00kQpucdid",
-};
